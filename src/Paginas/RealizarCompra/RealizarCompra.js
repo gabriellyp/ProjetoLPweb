82b077c57263af7c4ac3baeea670c026ec8e6796
gabriellyp <alanagaby11@hotmail.com>
2022-05-24 00:12:32 -0300

    realizar compra em andamento

--- src/Paginas/RealizarCompra/RealizarCompra.js
+++ src/Paginas/RealizarCompra/RealizarCompra.js
@@ -6,6 +6,18 @@ import {
     BreadcrumbLink,
     BreadcrumbSeparator,
   } from '@chakra-ui/react'
+  import { Carousel } from 'antd';
+  import Colarcoracao from '../../Assets/imagens/Colarcoracao.jpg'
+  import { Card } from 'antd';
+
+  const contentStyle = {
+    height: '400px',
+    color: '#fff',
+    width: '400px',
+    background: '#F0F0F0',
+  };
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);}
 
 export function RealizarCompra(){
     return(
@@ -19,14 +31,44 @@ export function RealizarCompra(){
                 </BreadcrumbItem>
 
                 <BreadcrumbItem>
-                <BreadcrumbLink href='#'>Docs</BreadcrumbLink>
+                <BreadcrumbLink href='#'>Colares</BreadcrumbLink>
                 </BreadcrumbItem>
 
                 <BreadcrumbItem isCurrentPage>
-                <BreadcrumbLink href='#'>Breadcrumb</BreadcrumbLink>
+                <BreadcrumbLink href='#'>Realizar Compra</BreadcrumbLink>
                 </BreadcrumbItem>
                 </Breadcrumb>
             </div>
+            <div className='container-info-produtos'>
+                <div className='info-produtos'>
+                    <Carousel afterChange={onChange} autoplay>
+                        <div>
+                            <h3 style={contentStyle}><img className="imagem-produtos-info" src={Colarcoracao}/></h3>
+                        </div>
+                        <div>
+                            <h3 style={contentStyle}><img src={Colarcoracao}/></h3>
+                        </div>
+                    </Carousel>
+                </div>
+                <div className='card-info'>
+                    <div className="site-card-border-less-wrapper">
+                        <Card
+                        title="Colar dois amores"
+                        bordered={false}
+                        style={{
+                            width: 300,
+                        }}
+                        >
+                        <p>R$ 90</p>   
+                        <p>DESCRIÇÃO DO PRODUTO</p>
+                        <p>Nome: Colar dois amores</p>
+                        <p>Material: Prata 925</p>
+                        <p>Tamanho: 30 cm de comprimento</p>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
